9e9b21f3fe1cb1653d952698d2f15c84da5b9572
Hamdrive <hamza.husein017@gmail.com>
2022-10-30 10:15:40 +0530

    [feat] navbar UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 import { AddRoute, Home, ViewAllRoutes, ViewSingleRoute } from './pages';
+import {FaHome} from 'react-icons/fa'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="routes" element={<ViewAllRoutes />} />
-      <Route path="routes/:routeId" element={<ViewSingleRoute />} />
-      <Route path="addroute" element={<AddRoute />} />
-    </Routes>
+    <>
+      <header className='navbar'>
+        <section className='navbar_home'>
+          <FaHome />
+        </section>
+        <section className="navbar_heading">
+          <h2>Traffic se bachte hai</h2>
+        </section>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="routes" element={<ViewAllRoutes />} />
+        <Route path="routes/:routeId" element={<ViewSingleRoute />} />
+        <Route path="addroute" element={<AddRoute />} />
+      </Routes>
+    </>
   );
 }
 
