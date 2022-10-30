ac1f83f55e3c524be839360d1678c92bed346635
Hamdrive <hamza.husein017@gmail.com>
2022-10-30 22:35:40 +0530

    [update] implement logic for adding route

--- src/pages/AddRoute.jsx
+++ src/pages/AddRoute.jsx
@@ -2,13 +2,88 @@ import React from "react";
 import { useState } from "react";
 import { FaGripLines, FaPlusCircle } from "react-icons/fa";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useRoute } from "../context/route-context";
+import { useLocation, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect } from "react";
 
 const AddRoute = () => {
-  const [stops, setStops] = useState([
-    { id: "1", stopName: "A", latitude: 18.949127, longitude: 72.83477 },
-    { id: "2", stopName: "B", latitude: 19.12498, longitude: 72.916664 },
-    { id: "3", stopName: "C", latitude: 19.229732, longitude: 72.85623 },
-  ]);
+  const [newRoute, setNewRoute] = useState([]);
+
+  const { dispatch } = useRoute();
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const { updateExistingRoute } = state;
+
+  const handleAddStop = () => {
+    if (newRoute.route.stops.length === 0) {
+      setNewRoute({
+        id: uuidv4(),
+        routeName: "",
+        routeDirection: "",
+        routeStatus: "",
+        stops: [{ stopId: "1", stopName: "", stopLat: 0, stopLon: 0 }],
+      });
+    } else {
+      setNewRoute((prevRoute) => ({
+        ...prevRoute,
+        stops: [
+          ...prevRoute.stops,
+          {
+            stopId: `${prevRoute.stops.length + 1}`,
+            stopName: "",
+            stopLat: 0,
+            stopLon: 0,
+          },
+        ],
+      }));
+    }
+  };
+
+  const handleUpdateRouteName = (updatedRouteName) => {
+    setNewRoute((prevRoute) => ({ ...prevRoute, routeName: updatedRouteName }));
+  };
+
+  const handleUpdateRouteDirection = (updatedRouteDirection) => {
+    setNewRoute((prevRoute) => ({
+      ...prevRoute,
+      routeName: updatedRouteDirection,
+    }));
+  };
+
+  const handleUpdateRouteStatus = (updatedRouteStatus) => {
+    setNewRoute((prevRoute) => ({
+      ...prevRoute,
+      routeName: updatedRouteStatus,
+    }));
+  };
+
+  const handleUpdateStopName = (currentStopId, updatedStopName) => {
+    const updatedStop = newRoute.stops.map((stop) =>
+      stop.id === currentStopId ? { ...stop, stopName: updatedStopName } : stop
+    );
+    setNewRoute((prevRoute) => ({ ...prevRoute, stops: updatedStop }));
+  };
+
+  const handleUpdateStopLat = (currentStopId, updatedStopLat) => {
+    const updatedStop = newRoute.stops.map((stop) =>
+      stop.id === currentStopId ? { ...stop, stopLat: updatedStopLat } : stop
+    );
+    setNewRoute((prevRoute) => ({ ...prevRoute, stops: updatedStop }));
+  };
+
+  const handleUpdateStopLon = (currentStopId, updatedStopLon) => {
+    const updatedStop = newRoute.stops.map((stop) =>
+      stop.id === currentStopId ? { ...stop, stopLon: updatedStopLon } : stop
+    );
+    setNewRoute((prevRoute) => ({ ...prevRoute, stops: updatedStop }));
+  };
+
+  const handleNewRoute = (dispatchType) => {
+    dispatch({ type: dispatchType, payload: newRoute });
+    setNewRoute([]);
+    navigate("/routes");
+  };
 
   const handleOnDragEnd = (result) => {
     const { source, destination } = result;
@@ -16,7 +91,7 @@ const AddRoute = () => {
     if (source.index === destination.index) return;
 
     let currentStop = {},
-      currentStopsList = stops,
+      currentStopsList = newRoute.stops,
       newStopsList = [];
 
     currentStop = currentStopsList[source.index];
@@ -27,8 +102,13 @@ const AddRoute = () => {
       ...currentStopsList.splice(destination.index),
     ];
 
-    setStops(newStopsList);
+    setNewRoute(newStopsList);
   };
+
+  useEffect(() => {
+    if (updateExistingRoute) setNewRoute(updateExistingRoute);
+  }, []);
+
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <main>
@@ -39,10 +119,17 @@ const AddRoute = () => {
             id="route_name"
             className="route_name_input"
             placeholder="Enter route name"
+            onChange={(e) => handleUpdateRouteName(e.target.value)}
+            value={newRoute.routeName}
           />
           <section>
             <label htmlFor="route_direction">Choose Direction &nbsp;</label>
-            <select name="direction" id="route_direction">
+            <select
+              name="direction"
+              id="route_direction"
+              onChange={(e) => handleUpdateRouteDirection(e.target.value)}
+              value={newRoute.routeDirection}
+            >
               <option value="up" selected>
                 UP
               </option>
@@ -51,7 +138,12 @@ const AddRoute = () => {
           </section>
           <section>
             <label htmlFor="route_status">Choose Status &nbsp;</label>
-            <select name="status" id="route_status">
+            <select
+              name="status"
+              id="route_status"
+              onChange={(e) => handleUpdateRouteStatus(e.target.value)}
+              value={newRoute.routeStatus}
+            >
               <option value="active" selected>
                 ACTIVE
               </option>
@@ -64,6 +156,7 @@ const AddRoute = () => {
               name="add_stop"
               className="action_button"
               aria-label="Click to enter a new stop"
+              onClick={handleAddStop}
             >
               <FaPlusCircle className="plus_circle_icon" />
               Add Stop
@@ -71,56 +164,78 @@ const AddRoute = () => {
           </section>
         </section>
         <section className="stop_details_section">
-          <Droppable droppableId="StopsList">
-            {(provided) => (
-              <section ref={provided.innerRef} {...provided.droppableProps}>
-                {stops.map((stop, index) => (
-                  <Draggable key={stop.id} draggableId={stop.id} index={index}>
-                    {(provided) => (
-                      <section
-                        className="single_stop_details_section"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <FaGripLines className="stop_grip_lines" />
-                        {/* <p>{stop.stopName}</p> */}
-                        <input
-                          type="text"
-                          name=""
-                          id=""
-                          placeholder="Enter stop name"
-                        />
-                        <input
-                          type="text"
-                          name=""
-                          id=""
-                          placeholder="Enter latitude"
-                        />
-                        <input
-                          type="text"
-                          name=""
-                          id=""
-                          placeholder="Enter longitude"
-                        />
-                      </section>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </section>
-            )}
-          </Droppable>
+          {newRoute.stops.length > 0 ? (
+            <Droppable droppableId="StopsList">
+              {(provided) => (
+                <section ref={provided.innerRef} {...provided.droppableProps}>
+                  {newRoute.stops.map((stop, index) => (
+                    <Draggable
+                      key={stop.id}
+                      draggableId={stop.id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <section
+                          className="single_stop_details_section"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <FaGripLines className="stop_grip_lines" />
+                          {/* <p>{stop.stopName}</p> */}
+                          <input
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Enter stop name"
+                            onChange={(e) =>
+                              handleUpdateStopName(stop.id, e.target.value)
+                            }
+                          />
+                          <input
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Enter stop latitude"
+                            onChange={(e) =>
+                              handleUpdateStopLat(stop.id, e.target.value)
+                            }
+                          />
+                          <input
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Enter stop longitude"
+                            onChange={(e) =>
+                              handleUpdateStopLon(stop.id, e.target.value)
+                            }
+                          />
+                        </section>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </section>
+              )}
+            </Droppable>
+          ) : (
+            <p className="empty_route_message">Add a stop to get started!</p>
+          )}
         </section>
         <section>
-          <button
-            name="submit_route"
-            id="submit_route"
-            className="action_button"
-            aria-label="Click to submit route"
-          >
-            Submit Route
-          </button>
+          {newRoute.stops.length > 0 && (
+            <button
+              name="submit_route"
+              id="submit_route"
+              className="action_button"
+              aria-label="Click to submit route"
+              onClick={() =>
+                updateExistingRoute ? handleNewRoute("UPDATE_ROUTE") : handleNewRoute("ADD_ROUTE")
+              }
+            >
+              {updateExistingRoute ? "Update Route" : "Submit Route"}
+            </button>
+          )}
         </section>
       </main>
     </DragDropContext>
